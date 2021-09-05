a08c982bd0c873724f810aad92d3a00ef99d1678
Alvaro Isea <alvaroe9448@gmail.com>
2021-09-05 13:46:06 +0200

    created syncDeletedNotes use-case, passed test

--- app/src/main/java/com/codingwithmitch/cleannotes/business/interactors/splash/SyncDeletedNotes.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/business/interactors/splash/SyncDeletedNotes.kt
@@ -1,4 +1,74 @@
 package com.codingwithmitch.cleannotes.business.interactors.splash
 
-class SyncDeletedNotes {
+import com.codingwithmitch.cleannotes.business.data.cache.CacheResponseHandler
+import com.codingwithmitch.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
+import com.codingwithmitch.cleannotes.business.data.remote.ApiResponseHandler
+import com.codingwithmitch.cleannotes.business.data.remote.abstraction.NoteRemoteDataSource
+import com.codingwithmitch.cleannotes.business.data.uitl.safeApiCall
+import com.codingwithmitch.cleannotes.business.data.uitl.safeCacheCall
+import com.codingwithmitch.cleannotes.business.domain.model.Note
+import com.codingwithmitch.cleannotes.business.domain.state.DataState
+import com.codingwithmitch.cleannotes.business.domain.state.StateEvent
+import com.codingwithmitch.cleannotes.util.printLogD
+import kotlinx.coroutines.Dispatchers
+
+/*
+    Search firestore for all notes in the "deleted" node.
+    It will then search the cache for notes matching those deleted notes.
+    If a match is found, it is deleted from the cache.
+ */
+class SyncDeletedNotes(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteRemoteDataSource: NoteRemoteDataSource
+){
+
+    suspend fun syncDeletedNotes(){
+
+        val apiResult = safeApiCall(Dispatchers.IO){
+            noteRemoteDataSource.getDeletedNotes()
+        }
+        val response = object: ApiResponseHandler<List<Note>, List<Note>>(
+            response = apiResult,
+            stateEvent = null
+        ){
+            override suspend fun handleSuccess(resultObj: List<Note>): DataState<List<Note>>? {
+                return DataState.data(
+                    response = null,
+                    data = resultObj,
+                    stateEvent = null
+                )
+            }
+        }
+
+        val notes = response.getResult()?.data?: ArrayList()
+
+        val cacheResult = safeCacheCall(Dispatchers.IO){
+            noteCacheDataSource.deleteNotes(notes)
+        }
+
+        object: CacheResponseHandler<Int, Int>(
+            response = cacheResult,
+            stateEvent = null
+        ){
+            override suspend fun handleSuccess(resultObj: Int): DataState<Int>? {
+                return processSuccessfulResponse( resultObj, null)
+            }
+
+            override fun processSuccessfulResponse(
+                resultObj: Int,
+                stateEvent: StateEvent?
+            ): DataState<Int>? {
+                printLogD("SyncNotes",
+                    "num deleted notes: ${resultObj}")
+                return DataState.data(
+                    response = null,
+                    data = resultObj,
+                    stateEvent = null
+                )
+            }
+        }.execute()
+
+    }
+
+
 }
---
+++ app/src/test/java/com/codingwithmitch/cleannotes/business/interactors/splash/SyncDeletedNotesTest.kt
@@ -0,0 +1,61 @@
+package com.codingwithmitch.cleannotes.business.interactors.splash
+
+import com.codingwithmitch.cleannotes.business.domain.model.Note
+import com.codingwithmitch.cleannotes.business.interactors.BaseUseCaseToolsTest
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+/*
+Test cases:
+1. deleteNetworkNotes_confirmCacheSync()
+    a) select some notes for deleting from network
+    b) delete from network
+    c) perform sync
+    d) confirm notes from cache were deleted
+ */
+
+@InternalCoroutinesApi
+class SyncDeletedNotesTest : BaseUseCaseToolsTest() {
+
+    //system in test
+    private lateinit var syncDeletedNotes : SyncDeletedNotes
+
+
+    init {
+        initSystemInTest()
+    }
+
+
+    override fun initSystemInTest() {
+         syncDeletedNotes = SyncDeletedNotes(
+             noteCacheDataSource = noteCacheDataSource,
+             noteRemoteDataSource = noteRemoteDataSource
+         )
+    }
+
+    @Test
+    fun deleteNetworkNotes_confirmCacheSync() = runBlocking {
+
+        // select some notes to be deleted from cache
+        val networkNotes = noteRemoteDataSource.getAllNotes()
+        val notesToDelete: ArrayList<Note> = ArrayList()
+        for(note in networkNotes){
+            notesToDelete.add(note)
+            noteRemoteDataSource.deleteNote(note.id)
+            if(notesToDelete.size > 3){
+                break
+            }
+        }
+
+        // perform sync
+        syncDeletedNotes.syncDeletedNotes()
+
+        // confirm notes were deleted from cache
+        for(note in notesToDelete){
+            val cachedNote = noteCacheDataSource.searchNoteById(note.id)
+            assertTrue { cachedNote == null }
+        }
+    }
+}
